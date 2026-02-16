c5129864ab10a47182a74aeb62eb81e3ed0d2825
ASUS <ASUS@LAPTOP-69C6QKQG>
2026-02-16 17:43:24 +0530

    Test2 Tescase commit to Brach2

---
+++ src/FirstProgram/Test2.java
@@ -0,0 +1,27 @@
+package FirstProgram;
+
+public class Test2 {
+	public static void click() {
+		System.out.println("In click() with 0 arguments");
+	}
+	public void click(String WebElement) {
+		System.out.println("In click(String) with String arguments");
+		System.out.println("WebElement: "+WebElement);
+	}
+	public static void click(int xAxis, int yAxis) {
+		System.out.println("In click(int,int) with int arguments");
+		System.out.println("xAxis: "+xAxis);
+		System.out.println("yAxis: "+yAxis);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		click();
+		Overloading test = new Overloading();
+		test.click("Login Button");
+		click(1,2);
+		
+
+	}
+
+}

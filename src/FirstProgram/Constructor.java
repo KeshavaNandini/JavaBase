af44234b4718aaaa3e94f6bc493a435a8c6b41b7
61070290 <61070290@9DYV7R3>
2026-02-16 16:39:58 +0530

    FIRST COMMIT...

---
+++ src/FirstProgram/Constructor.java
@@ -0,0 +1,12 @@
+package FirstProgram;
+
+public class Constructor {
+	public Constructor() {
+
+	}
+
+	public static void main(String[] args) {
+		Constructor test = new Constructor();
+	}
+
+}
---
+++ src/FirstProgram/Methods.java
@@ -0,0 +1,28 @@
+package FirstProgram;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		Methods ref = new Methods();
+		ref.m1();
+		// TODO Auto-generated method stub
+
+	}
+
+	void m1() {
+		m2();
+		System.out.println("In m1");
+	}
+
+	void m2() {
+		m3();
+		System.out.println("In m2");
+	}
+	
+	void m3() {
+		//m3();
+		System.out.println("In m3");
+	}
+	 
+
+}
